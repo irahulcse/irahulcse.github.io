0ac9123604ceca13a44b9cbb55d25d22b46f48a8
irahulcse <1rahulchandra1@gmail.com>
2020-08-15 19:33:10 +0530

    First Commit at 2020-08-15-19:33:10

--- src/portfolio.js
+++ src/portfolio.js
@@ -560,8 +560,8 @@ const experience = {
           company: "Google",
           company_url: "https://about.google/",
           logo_path: "google_logo.png",
-          duration: "June 2019 - April 2020",
-          location: "Hyderabad, Telangana",
+          duration: "June 18 - April 2019",
+          location: "Mohali, Chandigarh",
           description:
             "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
           color: "#4285F4",
@@ -571,8 +571,8 @@ const experience = {
           company: "Microsoft",
           company_url: "https://www.microsoft.com/",
           logo_path: "microsoft_logo.png",
-          duration: "Aug 2019 - May 2020",
-          location: "Hyderabad, Telangana",
+          duration: "Dec 2019 - July 2020",
+          location: "Chandigarh, Mohali",
           description:
             "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
           color: "#D83B01",
@@ -583,19 +583,19 @@ const experience = {
           company_url: "https://www.mozilla.org/",
           logo_path: "mozilla_logo.png",
           duration: "Oct 2019 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
+          location: "Chandigarh, Mohali, Punjab",
           description:
             "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
           color: "#000000",
         },
         {
           title: "Developer Students Club Member",
-          company: "DSC IIITDM Kurnool",
+          company: "GDG Jalandhar",
           company_url:
             "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
           logo_path: "dsc_logo.png",
           duration: "Jan 2018 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
+          location: "Jalandhar, Punjab",
           description:
             "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
           color: "#0C9D58",
