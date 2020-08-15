684f0fc5cdd3112244735c54e2fc16f73dc0952b
irahulcse <1rahulchandra1@gmail.com>
2020-08-15 16:58:42 +0530

    First Commit at 2020-08-15-16:58:42

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -36,6 +36,15 @@ export default function Greeting(props) {
                   className="portfolio-repo-btn"
                 />
               </div>
+              <div className="portfolio-repo-btn-div">
+                <Button
+                  text="⭐ Follow Me On Github"
+                  newTab={true}
+                  href={greeting.portfolio_repository}
+                  theme={theme}
+                  className="portfolio-repo-btn"
+                />
+              </div>
               {/* <div className="button-greeting-div">
               <Button text="Contact me" href="#contact" />
               <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
