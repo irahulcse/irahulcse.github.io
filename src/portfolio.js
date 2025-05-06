b82d745c4bebbe585a96a7b920d19f412c40f976
irahulcse <1rahulchandra1@gmail.com>
2025-05-06 10:09:08 +0200

    First Commit at 2025-05-06-10:09:08

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrive to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://www.dropbox.com/scl/fi/3qesnd9jsd11egx6yk75c/Rahul_Chandra_Resume_SDE_1_2024_english_new.pdf?rlkey=eus32hsr3zudukawvauquyq48&st=bg7hlzsj&dl=0",
+    "https://drive.google.com/file/d/1FwopmdWNqtmRj_uch5nEZHWLjoRlSSno/view?usp=drive_link",
   portfolio_repository: "https://github.com/irahulcse",
 };
 
